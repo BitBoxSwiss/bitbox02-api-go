2c13825d6e8218d4a6f0dc265a7e7314f48e29f4
Marko Bencun <marko@shiftcrypto.ch>
2019-11-21 22:05:00 +0100

    api/firmware: move mnemonic related functions to new file

--- api/firmware/device.go
+++ api/firmware/device.go
@@ -18,7 +18,6 @@ package firmware
 import (
 	"fmt"
 	"sync"
-	"time"
 
 	"github.com/digitalbitbox/bitbox02-api-go/api/common"
 	"github.com/digitalbitbox/bitbox02-api-go/api/firmware/messages"
@@ -460,27 +459,6 @@ func (device *Device) SetPassword() error {
 	return nil
 }
 
-// SetMnemonicPassphraseEnabled enables or disables entering a mnemonic passphrase after the normal
-// unlock.
-func (device *Device) SetMnemonicPassphraseEnabled(enabled bool) error {
-	request := &messages.Request{
-		Request: &messages.Request_SetMnemonicPassphraseEnabled{
-			SetMnemonicPassphraseEnabled: &messages.SetMnemonicPassphraseEnabledRequest{
-				Enabled: enabled,
-			},
-		},
-	}
-	response, err := device.query(request)
-	if err != nil {
-		return err
-	}
-	_, ok := response.Response.(*messages.Response_Success)
-	if !ok {
-		return errp.New("unexpected response")
-	}
-	return nil
-}
-
 func (device *Device) reboot() error {
 	request := &messages.Request{
 		Request: &messages.Request_Reboot{
@@ -508,48 +486,6 @@ func (device *Device) Reset() error {
 	}
 	_, err := device.query(request)
 	return err
-}
-
-// ShowMnemonic lets the user export the bip39 mnemonic phrase on the device.
-func (device *Device) ShowMnemonic() error {
-	request := &messages.Request{
-		Request: &messages.Request_ShowMnemonic{
-			ShowMnemonic: &messages.ShowMnemonicRequest{},
-		},
-	}
-	response, err := device.query(request)
-	if err != nil {
-		return err
-	}
-	_, ok := response.Response.(*messages.Response_Success)
-	if !ok {
-		return errp.New("unexpected response")
-	}
-	return nil
-}
-
-// RestoreFromMnemonic invokes the mnemonic phrase import workflow.
-func (device *Device) RestoreFromMnemonic() error {
-	now := time.Now()
-	_, offset := now.Zone()
-	request := &messages.Request{
-		Request: &messages.Request_RestoreFromMnemonic{
-			RestoreFromMnemonic: &messages.RestoreFromMnemonicRequest{
-				Timestamp:      uint32(now.Unix()),
-				TimezoneOffset: int32(offset),
-			},
-		},
-	}
-	response, err := device.query(request)
-	if err != nil {
-		return err
-	}
-	_, ok := response.Response.(*messages.Response_Success)
-	if !ok {
-		return errp.New("unexpected response")
-	}
-	device.changeStatus(StatusInitialized)
-	return nil
 }
 
 // Product returns the device product.
