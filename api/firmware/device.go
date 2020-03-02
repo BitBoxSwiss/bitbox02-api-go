ea905fe1e8a0a707dc86cd405c0f416e51eb57b8
Marko Bencun <marko@shiftcrypto.ch>
2020-03-02 14:06:43 +0100

    api/firmware: min version must be 6.1.0
    Contains a required bugfix for MEW and a required new api call for
    Electrum (ElectrumEncryptionKey).

--- api/firmware/device.go
+++ api/firmware/device.go
@@ -30,8 +30,8 @@ import (
 //go:generate sh -c "protoc --proto_path=messages/ --go_out='import_path=messages,paths=source_relative:messages' messages/*.proto"
 
 var (
-	lowestSupportedFirmwareVersion                   = semver.NewSemVer(6, 0, 0)
-	lowestSupportedFirmwareVersionBTCOnly            = semver.NewSemVer(6, 0, 0)
+	lowestSupportedFirmwareVersion                   = semver.NewSemVer(6, 1, 0)
+	lowestSupportedFirmwareVersionBTCOnly            = semver.NewSemVer(6, 1, 0)
 	lowestSupportedFirmwareVersionBitBoxBaseStandard = semver.NewSemVer(4, 3, 0)
 	lowestNonSupportedFirmwareVersion                = semver.NewSemVer(7, 0, 0)
 )
