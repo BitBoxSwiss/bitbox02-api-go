be449a9c53eda65389a64d19c1016bccb8380772
Marko Bencun <marko@shiftcrypto.ch>
2024-02-20 11:38:01 +0100

    api/firmware/bip85: rename BIP85 and add BIP85AppLN()
    Access to the BIP85 sub-apps BIP39 and LN according to
    https://github.com/digitalbitbox/bitbox02-firmware/pull/1179.

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -207,7 +207,6 @@ func main() {
 	info, err := device.DeviceInfo()
 	errpanic(err)
 	fmt.Printf("Device info: %+v", info)
-
 	//signFromTxID(device, "48e83b2a44c21dab01fc7bad0df1b1d7a59e48af79069454a8320ec6a9d1aefb")
 
 	sig, err := device.ETHSignEIP1559(
