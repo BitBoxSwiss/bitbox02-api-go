940a7a25b2110953d754c7a851089fc2313b21e7
Nikolas De Giorgis <nikolas.degiorgis@gmail.com>
2025-06-16 12:51:45 +0100

    tests: fix hash mismatch check in simulator tests.
    The test didn't check again for the hashes after downloading the file,
    so we add a further check.

--- api/firmware/device_test.go
+++ api/firmware/device_test.go
@@ -41,6 +41,7 @@ import (
 	"github.com/BitBoxSwiss/bitbox02-api-go/api/firmware/messages"
 	"github.com/BitBoxSwiss/bitbox02-api-go/api/firmware/mocks"
 	"github.com/BitBoxSwiss/bitbox02-api-go/communication/u2fhid"
+	"github.com/BitBoxSwiss/bitbox02-api-go/util/errp"
 	"github.com/BitBoxSwiss/bitbox02-api-go/util/semver"
 	"github.com/flynn/noise"
 	"github.com/stretchr/testify/require"
@@ -109,6 +110,15 @@ func downloadSimulators() ([]string, error) {
 		return nil, err
 	}
 
+	hashesMatch := func(file *os.File, expectedHash string) (bool, error) {
+		hasher := sha256.New()
+		if _, err := io.Copy(hasher, file); err != nil {
+			return false, err
+		}
+		actualHash := hex.EncodeToString(hasher.Sum(nil))
+		return actualHash == expectedHash, nil
+	}
+
 	fileNotExistOrHashMismatch := func(filename, expectedHash string) (bool, error) {
 		file, err := os.Open(filename)
 		if os.IsNotExist(err) {
@@ -119,13 +129,11 @@ func downloadSimulators() ([]string, error) {
 		}
 		defer file.Close()
 
-		hasher := sha256.New()
-		if _, err := io.Copy(hasher, file); err != nil {
+		match, err := hashesMatch(file, expectedHash)
+		if err != nil {
 			return false, err
 		}
-		actualHash := hex.EncodeToString(hasher.Sum(nil))
-
-		return actualHash != expectedHash, nil
+		return !match, nil
 	}
 
 	downloadFile := func(url, filename string) error {
@@ -164,12 +172,28 @@ func downloadSimulators() ([]string, error) {
 			return nil, err
 		}
 		if doDownload {
+			fmt.Printf("Downloading %s to %s\n", simulator.URL, filename)
 			if err := downloadFile(simulator.URL, filename); err != nil {
 				return nil, err
+			}
+			// If we downloaded the file, check again the hash.
+			file, err := os.Open(filename)
+			if err != nil {
+				// This should never happen, as we just downloaded it
+				return nil, err
+			}
+			match, err := hashesMatch(file, simulator.Sha256)
+			if err != nil {
+				return nil, err
+			}
+			if !match {
+				return nil, errp.Newf("downloaded file %s does not match expected hash %s", filename, simulator.Sha256)
 			}
 			if err := os.Chmod(filename, 0755); err != nil {
 				return nil, err
 			}
+		} else {
+			fmt.Printf("Skipping download of %s, file already exists and has the correct hash\n", filename)
 		}
 		filenames = append(filenames, filename)
 	}
